2324137accc52ad9a3912226245f4ff761fad87f
GrishchenkoN <rayc0r379@gmail.com>
2022-11-12 12:28:51 +0300

    Complited api with shit-json

--- wfapi.py
+++ wfapi.py
@@ -1,15 +1,14 @@
 from typing import Union
 
 from fastapi import FastAPI
+import json
+
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    with open("json-HW-IT.json") as rf:
+        data = json.load(rf)
+    return(data)
